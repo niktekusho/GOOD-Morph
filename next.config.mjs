656fe18b317e98150908886742ee3cdb682c378d
niktekusho <niktekusho@users.noreply.github.com>
2024-04-13 17:35:04 +0200

    Revert last commit (the actions/configure-pages should take care of it)
    This reverts commit 796c4428bf12687dd6d0ebaa438e91f4490a0971.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Enables static site generation for Github Pages
-  output: "export",
-  // On GitHub Pages, the default URL is https://{user}.github.com/{repo}/ so we need to adjust Next.js configuration for that additional {repo} path segment
-  basePath: "GOOD-Morph",
-};
+const nextConfig = {};
 
 export default nextConfig;

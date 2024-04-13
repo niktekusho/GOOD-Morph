796c4428bf12687dd6d0ebaa438e91f4490a0971
niktekusho <niktekusho@users.noreply.github.com>
2024-04-13 17:19:51 +0200

    Tries to fix some Next.js vs GitHub Pages things

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Enables static site generation for Github Pages
+  output: "export",
+  // On GitHub Pages, the default URL is https://{user}.github.com/{repo}/ so we need to adjust Next.js configuration for that additional {repo} path segment
+  basePath: "GOOD-Morph",
+};
 
 export default nextConfig;

5abe16c92fdd3a21229cc6b510471203c4389c8f
niktekusho <niktekusho@users.noreply.github.com>
2024-04-13 19:06:06 +0200

    Reverts previous commits
    The missing images seems (🤞) to be related to the absolute path used by the specific components.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Enables static site generation for Github Pages
-  output: "export",
-  // On GitHub Pages, the default URL is https://{user}.github.com/{repo}/ so we need to adjust Next.js configuration for that additional {repo} path segment
-  basePath: "/GOOD-Morph",
-  // Disable Next.js server-side image optimization
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;

fc29a2a0e3196bf46a7d3727c44faf58e2b491db
niktekusho <niktekusho@users.noreply.github.com>
2024-04-13 18:59:29 +0200

    Tries to fix the page failing to reference images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Enables static site generation for Github Pages
+  output: "export",
+  // On GitHub Pages, the default URL is https://{user}.github.com/{repo}/ so we need to adjust Next.js configuration for that additional {repo} path segment
+  basePath: "/GOOD-Morph",
+  // Disable Next.js server-side image optimization
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;

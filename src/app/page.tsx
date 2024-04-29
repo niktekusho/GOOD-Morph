8a27a9dd3d16e5a16e193781fd0cbdcfb5c37fd2
niktekusho <niktekusho@users.noreply.github.com>
2024-04-29 19:42:31 +0200

    Adds previous session check
    If the main page found a previous session in sessionStorage AND we're not in development, show the user a dialog.
    In development we want to directly access the Ruleset page without prompting the choice (we always want to resume previous session).

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { MouseEventHandler, useEffect, useRef, useState } from "react";
 import { InitialPage } from "../components/InitialPage";
 import { ArtifactsRulePage } from "@/components/ArtifactsRulePage";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 
 export type UploadedFile = {
   name: string;
@@ -12,8 +22,9 @@ export type UploadedFile = {
 export default function Component() {
   const [file, setFile] = useState<UploadedFile>();
 
-  // Check if sessionStorage has a previous session
-  useEffect(() => {
+  const [showModal, setShowModal] = useState(false);
+
+  const loadFileFromSessionStorage = () => {
     const maybeFileName = sessionStorage.getItem("fileName");
     if (maybeFileName) {
       const fileContent = sessionStorage.getItem("fileContent")!;
@@ -22,15 +33,60 @@ export default function Component() {
         name: maybeFileName,
       });
     }
+  };
+
+  useEffect(() => {
+    const maybeFileName = sessionStorage.getItem("fileName");
+    if (maybeFileName) {
+      // In dev we want to be faster, so we skip the modal
+      if (process.env.NODE_ENV === "development") {
+        loadFileFromSessionStorage();
+      } else {
+        setShowModal(true);
+      }
+    }
   }, []);
 
+  const handleStartNewSessionBtn: MouseEventHandler = (_) => {
+    console.log("Clearing sessionStorage");
+    setShowModal(false);
+    sessionStorage.clear();
+  };
+
+  const handleContinuePreviousSessionBtn: MouseEventHandler = (_) => {
+    setShowModal(false);
+    loadFileFromSessionStorage();
+  };
+
   return (
-    <main className="min-h-[100dvh] grid p-2">
-      {file ? (
-        <ArtifactsRulePage file={file} />
-      ) : (
-        <InitialPage setFile={setFile} />
-      )}
-    </main>
+    <>
+      <AlertDialog open={showModal}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Welcome Back!</AlertDialogTitle>
+            <AlertDialogDescription>
+              It looks like you had a previous session that wasn&apos;t
+              completed.<br></br>Would you like to start a new session or
+              continue where you left off?
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={handleStartNewSessionBtn}>
+              Start New Session
+            </AlertDialogCancel>
+            <AlertDialogAction onClick={handleContinuePreviousSessionBtn}>
+              Continue Previous Session
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <main className="min-h-[100dvh] grid p-2">
+        {file ? (
+          <ArtifactsRulePage file={file} />
+        ) : (
+          <InitialPage setFile={setFile} />
+        )}
+      </main>
+    </>
   );
 }
